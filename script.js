1f706b158b1d5877121a1ea73001668d415fd800
Matt Bizarria <108092128+mattbizarria@users.noreply.github.com>
2022-10-10 12:06:33 -0300

    Get choice from user input

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function getPlayerChoice() {
+	while (true) {
+		let playerChoice = prompt('Rock, paper or scissors?')
+
+		if (playerChoice == null) {
+			continue
+		}
+		
+		playerChoice = playerChoice.toLowerCase()
+		
+		if (playerChoice === 'rock' ||
+			playerChoice === 'paper' ||
+			playerChoice === 'scissors') {
+				return playerChoice
+		}
+	}
+}
+
+
 function getComputerChoice() {
 	randomChoice = Math.floor(Math.random() * 3)
 
